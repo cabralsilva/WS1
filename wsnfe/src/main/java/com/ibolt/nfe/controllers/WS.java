353bfbbfc8c0d2e6c9ae774250890991940533ac
IBOLT <IBOLT@IBOLTCOMP_2>
2016-10-25 11:49:05 -0200

    COMENTARIO

--- wsnfe/src/main/java/com/ibolt/nfe/controllers/WS.java
+++ wsnfe/src/main/java/com/ibolt/nfe/controllers/WS.java
@@ -13,7 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-
+//	COMENTARIO
 @Path(value="/")
 public class WS {
     @POST
